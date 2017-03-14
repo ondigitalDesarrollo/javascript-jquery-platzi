2b1d6012ce730658ab0d33719f52ed8d2a65a3bb
Desarrollo Ondigital <ondigital.desarrollo@gmail.com>
2017-03-14 16:51:56 -0500

    Assets y estilos generales de la aplicacion

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+/* Vanilla Javascript 
+
+window.addEventListener('load', function(){
+alert('Ondigital');
+});
+
+ jQuery 
+
+$(function(){
+	//alert('Ondigital');
+});
+
+
+ jQuery no Conflict 
+$.noConflict();
+jQuery(document).ready(function($) {
+	console.log('Hello');	
+});
+
+*/
+
+$(function(){
+
+});
